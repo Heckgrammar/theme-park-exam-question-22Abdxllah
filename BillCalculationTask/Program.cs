459da32237fabdf7a462296d978e03b0c77de916
22Abdxllah <22Adalal@heckgrammar.co.uk>
2025-10-21 13:28:45 +0100

    Update Program.cs

--- BillCalculationTask/Program.cs
+++ BillCalculationTask/Program.cs
@@ -21,7 +21,7 @@
             // Commit your changes and add screnshot evidence of your program running to the Readme
 
             Console.WriteLine("How many people are in your group");
-            int people = int.Parse(Console.ReadLine());
+            int people = Conver.ToInt32(Console.ReadLine());
             int totalCharge = 0;
 
             if (people >= 6)
